7f54e52c1c50f23d3ed4c6fd0e121a1a5d84e51c
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-09 23:45:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Type Conversion
+#Teaches you how to convert the type
+#type = tells you the kind of data type
+
+print(type(str(100))) #This is a string because it ask for string of 100
+
+print(type(int(str(100)))) #this is a int because first you look for string of 100 then convert it to a integer
